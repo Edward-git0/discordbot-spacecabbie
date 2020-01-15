a308354b11c9777dc9f42f82bcc6684f5c88b7f4
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 18:46:43 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ for (const file of moduleFiles) {
     client.modules.set(_module.name, _module);
 } */
 
-client.mongoose = require('./utils/mongoose');
+client.mongoose = require('./utils/mongoose.js');
 
 client.on('ready', () => {
     console.log("Update");
