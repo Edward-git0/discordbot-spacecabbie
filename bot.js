8e16b0b1591c94a6ef6a82826b5bdca062fbc3f8
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 14:33:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,16 +10,17 @@ client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split(' ');
     const command = args.shift().toLowerCase();
-    
-    if(command == "ping") {
-        message.reply("Pong!");   
-    }
-    
-    if (!args.length) {
-		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-	}
 
-	message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    switch(command) {
+        case "ping":
+            return message.reply("Pong!");
+        break;
+        case "avatar":
+            if (!message.mentions.users.size) {
+                return message.channel.send(`Your avatar: <${message.author.displayAvatarURL}>`);
+            }
+        break;
+    }
 });
 
  
