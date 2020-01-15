ee5b493bdd447f5f537e47987dd9949358977b4c
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 17:06:41 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -49,7 +49,9 @@ client.on('message', message => {
             embed.setDescription("Bot Details");
             embed.addField("Uptime: ", uptime);
             embed.addField("Ping: ", client.ping+"ms");
-            embed.addBlankField();
+            embed.addField("Voice Connections: ", client.voiceConnections);
+            embed.addField("Status", client.status);
+            embed.addField("Alive", "Very");
             embed.setThumbnail("https://cdn.discordapp.com/avatars/667074896089579532/d8bc04cecf328a8f5516378e04d96e62.webp");
             embed.setFooter("Space Cabbie made by Edward", "https://cdn.discordapp.com/avatars/667074896089579532/d8bc04cecf328a8f5516378e04d96e62.webp");
 
