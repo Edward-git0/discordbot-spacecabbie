24ce9782efba7445c0ec3c70f64ef5d585eac23a
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 16:03:31 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,17 +6,9 @@ const { prefix, token, version } = require('./config.json');
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.modules = new Discord.Collection();
-const moduleFiles = fs.readdirSync('./modules').filter(file => file.endsWith('js'));
-
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	client.commands.set(command.name, command);
-}
-
-for (const file of moduleFiles) {
-    const module1 = require(`./modules/${file}`);
-    client.modules.set(module1.name, module1);
 }
 
 client.on('ready', () => {
@@ -31,16 +23,24 @@ client.on('message', message => {
 
     switch(command) {
         case "ping":
-            const embedDetails = {
-                "setTitle": "Title",
-                "setDescription": "Description"
-            }
-            const embed = client.modules.get('embed').execute();
-            client.channels.get('667088501895856178').send({embed});
             client.commands.get('ping').execute(message, args);
         break;
         case "avatar":
             client.commands.get('avatar').execute(message, args);
+        break;
+        case "embed":
+            const embed = new Discord.RichEmbed()
+            .setColor('#0099ff')
+            .setTitle('Some title')
+            .setURL('https://discord.js.org/')
+            .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+            .setDescription('Some description here')
+            .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+            .addBlankField()
+            .setImage('https://i.imgur.com/wSTFkRM.png')
+            .setTimestamp()
+            .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+            message.channel.send({embed});
         break;
         case "test": message.channel.send("Alive"); break;
     }
