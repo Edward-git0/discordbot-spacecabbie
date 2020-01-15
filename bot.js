5283046b3160ff24c9ef8ca418f442ff486f14fe
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 18:09:35 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,32 +14,6 @@ for (const file of commandFiles) {
 client.on('ready', () => {
     console.log("Update");
     client.channels.get('667088501895856178').send("Updated!");
-
-    try {
-        const mongoose = require('./mongoose');
-        const dbOptions = {
-            useNewUrlParser: true,
-            autoIndex: false,
-            reconnectTries: Number.MAX_VALUE,
-            reconnectInterval: 500,
-            poolSize: 5,
-            connectTimeoutMS: 10000,
-            family: 4
-        };
-        mongoose.connect("mongodb://space_cabbie:Cabbie111222@ds259207.mlab.com:59207/heroku_sf7429h4", dbOptions);
-        mongoose.set("useFindAndModify", false);
-        mongoose.Promise = global.Promise;
-
-        mongoose.connection.on("connected", () => {
-            console.log("Connected");
-            client.channels.get('667088501895856178').send("Connected!");
-        })
-    } catch(err) {
-        client.channels.get('667088501895856178').send(err);
-    }
-    mongoose.connect.on('err', () => {
-        client.channels.get('667088501895856178').send("Error connecting.");
-    })
 });
 
 client.on('message', message => {
