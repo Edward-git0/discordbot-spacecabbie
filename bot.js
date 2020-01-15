f2e87fd79e358b307cd90818eac1d58595a87e1a
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 15:51:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,11 +24,6 @@ client.on('ready', () => {
     client.channels.get('667088501895856178').send("Updated!");
 });
 
-client.on('reconnecting', () => {
-    console.log("reconnecting");
-    client.channels.get('667088501895856178').send("Updating.");
-});
-
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
     const args = message.content.slice(prefix.length).split('/ +/');
