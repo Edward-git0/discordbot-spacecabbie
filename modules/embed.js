6a013b0a17763f42df981edc345d625d9efa7d42
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 15:31:52 -0500

    Update embed.js

--- modules/embed.js
+++ modules/embed.js
@@ -19,7 +19,21 @@ module.exports = {
     name: 'embed',
     description: 'embed',
     execute(args) {
-        const embed = new Discord.RichEmbed();
-        return args.toString();
+        const embed = new Discord.RichEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setURL('https://discord.js.org/')
+        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+        .setDescription('Some description here')
+        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+        .addField('Regular field title', 'Some value here')
+        .addBlankField()
+        .addField('Inline field title', 'Some value here', true)
+        .addField('Inline field title', 'Some value here', true)
+        .addField('Inline field title', 'Some value here', true)
+        .setImage('https://i.imgur.com/wSTFkRM.png')
+        .setTimestamp()
+        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+        return embed;
     },
 };
