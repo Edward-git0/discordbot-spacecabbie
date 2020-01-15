ed77331b51c5177ac51bd10605110c4adcc7089d
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 14:19:25 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '.';
 
 client.on('ready', () => {
     client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-       message.reply('pong');
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    const args = message.content.slice(prefix.length).split(' ');
+    const command = args.shift().toLowerCase();
+    
+    if(command == "ping") {
+        message.reply("Pong!");   
     }
 });
 
