81fdba1c8060214cb623d19876032e51d591eb36
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 15:59:13 -0500

    Update embed.js

--- modules/embed.js
+++ modules/embed.js
@@ -1,20 +1,3 @@
-/* const embed = new Discord.RichEmbed()
-	.setColor('#0099ff')
-	.setTitle('Some title')
-	.setURL('https://discord.js.org/')
-	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
-	.setDescription('Some description here')
-	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
-	.addField('Regular field title', 'Some value here')
-	.addBlankField()
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.addField('Inline field title', 'Some value here', true)
-	.setImage('https://i.imgur.com/wSTFkRM.png')
-	.setTimestamp()
-    .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png'); */
-  
-    
 module.exports = {
     name: 'embed',
     description: 'embed',
@@ -26,11 +9,7 @@ module.exports = {
         .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
         .setDescription('Some description here')
         .setThumbnail('https://i.imgur.com/wSTFkRM.png')
-        .addField('Regular field title', 'Some value here')
         .addBlankField()
-        .addField('Inline field title', 'Some value here', true)
-        .addField('Inline field title', 'Some value here', true)
-        .addField('Inline field title', 'Some value here', true)
         .setImage('https://i.imgur.com/wSTFkRM.png')
         .setTimestamp()
         .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
