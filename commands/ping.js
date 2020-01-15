d316e0a69924392915e440831842516c724dce13
Edward-git0 <54463935+Edward-git0@users.noreply.github.com>
2020-01-15 16:33:39 -0500

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message, args) {
-        const ping = tostring(args).substring(1,6);
-        message.reply('Pong! ('+ping+'ms)')
+        //const ping = tostring(args).substring(1,6);
+        message.reply('Pong! ('+args+'ms)')
 	},
 };
